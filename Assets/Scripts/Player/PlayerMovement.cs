3d34f15f123048218259a479302d78bc0d24f0dd
Lanpasto <77079137+Lanpasto@users.noreply.github.com>
2024-10-09 10:32:59 +0300

    Hot fix v.7

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -71,7 +71,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void AddClickPosition()
     {
-        Vector3 clickPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+        Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
         clickPosition.z = 0;
 
         if (!clickPoints.ContainsKey(clickPosition))
@@ -81,6 +81,7 @@ public class PlayerMovement : MonoBehaviour
             flagManager.PutFlag(clickPosition);
         }
     }
+
 
     private void MoveToTarget(Vector3 target)
     {
